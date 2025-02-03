555d72006cb2d90950deba2c9deb9c50a7541ab9
Khashayar Barooti <khashayar.baroot@epfl.ch>
2025-02-03 15:27:03 +0000

    formatter

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 // use base64ct::{Base64, Encoding};
 use num_bigint::{BigUint, RandomBits};
 use num_traits::Num;
@@ -25,40 +24,40 @@ const BARRETT_REDUCTION_OVERFLOW_BITS: usize = 4;
  */
 pub fn compute_barrett_reduction_parameter(modulus: &BigUint) -> BigUint {
     let k = modulus.bits();
-    let multiplicand = BigUint::new([1].to_vec()) << (k as usize * 2 + BARRETT_REDUCTION_OVERFLOW_BITS);
+    let multiplicand =
+        BigUint::new([1].to_vec()) << (k as usize * 2 + BARRETT_REDUCTION_OVERFLOW_BITS);
     let barrett_reduction_parameter: BigUint = (multiplicand) / modulus;
 
+    //    let x: &str = "0x17f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb";
+    //    let x = bignum_from_string(x.to_string());
 
-//    let x: &str = "0x17f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb";
-//    let x = bignum_from_string(x.to_string());
+    //     let xxx = x.clone() + x.clone() + x.clone();
+    //     let x = x.clone();
+    //     let xxx_squared = xxx * x;
 
-//     let xxx = x.clone() + x.clone() + x.clone();
-//     let x = x.clone();
-//     let xxx_squared = xxx * x;
+    //     let x_mul_redc = xxx_squared.clone() * barrett_reduction_parameter.clone();
+    //     let shifted = x_mul_redc >> (k + k + BARRETT_REDUCTION_OVERFLOW_BITS);
 
-//     let x_mul_redc = xxx_squared.clone() * barrett_reduction_parameter.clone();
-//     let shifted = x_mul_redc >> (k + k + BARRETT_REDUCTION_OVERFLOW_BITS);
+    //     let quotient_mul_modulus = shifted * modulus.clone();
 
-//     let quotient_mul_modulus = shifted * modulus.clone();
+    //     let mut remainder_initial =  xxx_squared.clone() -quotient_mul_modulus.clone();
 
-//     let mut remainder_initial =  xxx_squared.clone() -quotient_mul_modulus.clone();
-
-//     if (remainder_initial.clone() >= modulus.clone())
-//     {
-//         remainder_initial = remainder_initial - modulus.clone();
-//     }
-//     // let ff = remainder_initial.as
-//     let bytes = remainder_initial.to_bytes_be();
-//    let encoded = hex::encode(&bytes);
-//    let mut r: String = String::from("");
-//    r += &format!("0x{}, ", hex::encode(&bytes));
-//     println!("REMAINDER = {remainder_initial}");
-//     println!("formatted = {r}");
+    //     if (remainder_initial.clone() >= modulus.clone())
+    //     {
+    //         remainder_initial = remainder_initial - modulus.clone();
+    //     }
+    //     // let ff = remainder_initial.as
+    //     let bytes = remainder_initial.to_bytes_be();
+    //    let encoded = hex::encode(&bytes);
+    //    let mut r: String = String::from("");
+    //    r += &format!("0x{}, ", hex::encode(&bytes));
+    //     println!("REMAINDER = {remainder_initial}");
+    //     println!("formatted = {r}");
     barrett_reduction_parameter
 }
 
 /**
- * @brief split a BigUint into a vector of 120-bit slices 
+ * @brief split a BigUint into a vector of 120-bit slices
  */
 pub fn split_into_120_bit_limbs(_input: &BigUint, num_bits: usize) -> Vec<BigUint> {
     let num_limbs: usize = (num_bits / 120) + (num_bits % 120 != 0) as usize;
@@ -76,14 +75,17 @@ pub fn split_into_120_bit_limbs(_input: &BigUint, num_bits: usize) -> Vec<BigUin
 }
 
 /**
- * @brief split a BigUint into a vector of 60-bit slices 
- * 
+ * @brief split a BigUint into a vector of 60-bit slices
+ *
  * @details the 60-bit slices represent a noir U60Repr object. NUM_CHUNKS reqpresents the size-multiplier of the U60Repr
  *          (either 2 or 4)
  */
-pub fn split_into_60_bit_limbs<const NUM_CHUNKS: usize>(_input: &BigUint, num_bits: usize) -> [Vec<BigUint>; NUM_CHUNKS] {
+pub fn split_into_60_bit_limbs<const NUM_CHUNKS: usize>(
+    _input: &BigUint,
+    num_bits: usize,
+) -> [Vec<BigUint>; NUM_CHUNKS] {
     let num_limbs: usize = (num_bits / 120) + (num_bits % 120 != 0) as usize;
-     let mut input = _input.clone();
+    let mut input = _input.clone();
     let one = BigUint::from(1 as u64);
     let mask: BigUint = (one.clone() << 60 as usize) - one.clone();
 
@@ -119,12 +121,15 @@ fn compute_bn_instance_parameters(modulus: &BigUint, num_bits: usize) -> BNInsta
     }
 }
 
-
-
 /**
  * @brief given a BNInstance, construct a string that represents noir code that defines a BigNumInstance object
  */
-fn compute_bn_instance_string(num_bits: usize, instance: &BNInstance, name: String, underscore_split: bool) -> String {
+fn compute_bn_instance_string(
+    num_bits: usize,
+    instance: &BNInstance,
+    name: String,
+    underscore_split: bool,
+) -> String {
     let BNInstance {
         has_multiplicative_inverse,
         modulus,
@@ -139,15 +144,15 @@ fn compute_bn_instance_string(num_bits: usize, instance: &BNInstance, name: Stri
     let limbs: String = String::from(itoa::Buffer::new().format(num_limbs as u64));
 
     let mut params: String = String::from("Params");
-    if underscore_split
-    {
-        params= String::from("_Params");
+    if underscore_split {
+        params = String::from("_Params");
     }
 
     let tparam: String = name.clone() + &params;
 
     let mut param_str: String = String::from("");
-    param_str += &String::from(format!("
+    param_str += &String::from(format!(
+        "
 use crate::params::BigNumParams;
 use crate::params::BigNumParamsGetter;
 use crate::utils::u60_representation::U60Repr;
@@ -158,15 +163,21 @@ impl BigNumParamsGetter<{},{}> for {}{} {{
     fn get_params() -> BigNumParams<{}, {}> {{
         {}_PARAMS
     }}
-    }}", name, params, limbs, bits, name, params, limbs, bits, name));
+    }}",
+        name, params, limbs, bits, name, params, limbs, bits, name
+    ));
 
     let mut r: String = String::from("");
-    r += &String::from(format!("pub global {}_PARAMS: BigNumParams<{}, {}> = BigNumParams {{
+    r += &String::from(format!(
+        "pub global {}_PARAMS: BigNumParams<{}, {}> = BigNumParams {{
         has_multiplicative_inverse: {},
         modulus: [
             ",
-        name.as_str(), limbs, bits, has_multiplicative_inverse)
-    );
+        name.as_str(),
+        limbs,
+        bits,
+        has_multiplicative_inverse
+    ));
     for i in 0..num_limbs - 1 {
         let bytes = modulus[i].to_bytes_be();
         r += &format!("0x{}, ", hex::encode(&bytes));
@@ -301,17 +312,26 @@ fn compute_runtime_bn_instance_string(
  *
  * @param bignum_str = base10 or base16 representation. base16 can be prepended with "0x"
  */
-pub fn bignum_from_string(
-    bignum_str: String
-) -> BigUint {
+pub fn bignum_from_string(bignum_str: String) -> BigUint {
     let mut formatted_bignum_str = bignum_str.clone();
     let mut base16 = false;
     if bignum_str.starts_with("0x") {
         formatted_bignum_str = bignum_str.split_once("0x").unwrap().1.to_string();
         base16 = true;
     }
-    if bignum_str.contains("a") | bignum_str.contains("b") | bignum_str.contains("c") |  bignum_str.contains("d") | bignum_str.contains("e") |  bignum_str.contains("f") |
-        bignum_str.contains("A") | bignum_str.contains("B") | bignum_str.contains("C") |  bignum_str.contains("D") | bignum_str.contains("E") |  bignum_str.contains("F") {
+    if bignum_str.contains("a")
+        | bignum_str.contains("b")
+        | bignum_str.contains("c")
+        | bignum_str.contains("d")
+        | bignum_str.contains("e")
+        | bignum_str.contains("f")
+        | bignum_str.contains("A")
+        | bignum_str.contains("B")
+        | bignum_str.contains("C")
+        | bignum_str.contains("D")
+        | bignum_str.contains("E")
+        | bignum_str.contains("F")
+    {
         base16 = true;
     }
     let mut base: u32 = 10;
@@ -329,50 +349,48 @@ pub fn bignum_from_string(
 /**
  * @brief Compute noir code for a runtime_bignum::BigNumInstance given a modulus String
  */
-pub fn bn_runtime_instance_from_string(
-    modulus_str: String,
-    name: String
-) -> String {
+pub fn bn_runtime_instance_from_string(modulus_str: String, name: String) -> String {
     let modulus = bignum_from_string(modulus_str);
     let num_bits = modulus.bits();
-    let result = compute_runtime_bn_instance_string(num_bits, compute_bn_instance_parameters(&modulus, num_bits), name);
+    let result = compute_runtime_bn_instance_string(
+        num_bits,
+        compute_bn_instance_parameters(&modulus, num_bits),
+        name,
+    );
     result
 }
 
 /**
  * @brief Compute noir code for a runtime_bignum::BigNumInstance given a modulus String
  */
-pub fn bn_runtime_instance(
-    modulus: BigUint,
-    num_bits: usize,
-    name: String
-) -> String {
-    let result = compute_runtime_bn_instance_string(num_bits, compute_bn_instance_parameters(&modulus, num_bits), name);
+pub fn bn_runtime_instance(modulus: BigUint, num_bits: usize, name: String) -> String {
+    let result = compute_runtime_bn_instance_string(
+        num_bits,
+        compute_bn_instance_parameters(&modulus, num_bits),
+        name,
+    );
     result
 }
-
 
 /**
  * @brief Compute noir code for a bignum::BigNumInstance given a modulus String
  */
-pub fn bn_instance_from_string(
-    modulus_str: String,
-    name: String,
-    is_uint: bool
-) -> String {
+pub fn bn_instance_from_string(modulus_str: String, name: String, is_uint: bool) -> String {
     let modulus = bignum_from_string(modulus_str);
     let num_bits = modulus.bits();
-    let result = compute_bn_instance_string(num_bits, &compute_bn_instance_parameters(&modulus, num_bits), name.clone(), !is_uint);
+    let result = compute_bn_instance_string(
+        num_bits,
+        &compute_bn_instance_parameters(&modulus, num_bits),
+        name.clone(),
+        !is_uint,
+    );
     result
 }
 
 /**
  * @brief Compute noir code for an array of 120-bit limbs that represents a BigNum object
  */
-pub fn bn_limbs_from_string(
-    bn_str: String
-) -> String {
-    
+pub fn bn_limbs_from_string(bn_str: String) -> String {
     let bn = bignum_from_string(bn_str);
     bn_limbs(bn.clone(), bn.bits())
 }
@@ -380,12 +398,9 @@ pub fn bn_limbs_from_string(
 /**
  * @brief Compute noir code for an array of 120-bit limbs that represents a BigNum object
  */
-pub fn bn_limbs(
-    bn: BigUint,
-    num_bits: usize
-) -> String {
+pub fn bn_limbs(bn: BigUint, num_bits: usize) -> String {
     let limbs = split_into_120_bit_limbs(&bn, num_bits);
-    
+
     let mut r = String::from("[");
     for i in 0..limbs.len() - 1 {
         let bytes = limbs[i].to_bytes_be();
@@ -399,24 +414,18 @@ pub fn bn_limbs(
 /**
  * @brief Compute noir code for an array of 120-bit limbs that represents a Barrett reduction parameter
  */
-pub fn redc_limbs_from_string(
-    bn_str: String,
-) -> String {
+pub fn redc_limbs_from_string(bn_str: String) -> String {
     let bn = bignum_from_string(bn_str);
     redc_limbs(bn.clone(), bn.bits())
 }
 
-
 /**
  * @brief Compute noir code for an array of 120-bit limbs that represents a Barrett reduction parameter
  */
-pub fn redc_limbs(
-    bn: BigUint,
-    num_bits: usize
-) -> String {
+pub fn redc_limbs(bn: BigUint, num_bits: usize) -> String {
     let redc_param = compute_barrett_reduction_parameter(&bn);
     let limbs = split_into_120_bit_limbs(&redc_param, num_bits);
-    
+
     let mut r = String::from("[");
     for i in 0..limbs.len() - 1 {
         let bytes = limbs[i].to_bytes_be();
@@ -426,7 +435,6 @@ pub fn redc_limbs(
     r += &String::from(format!("0x{}]", hex::encode(&bytes)));
     r
 }
-
 
 pub fn compute_double_modulus(modulus: &BigUint, num_bits: usize) -> Vec<BigUint> {
     let double_modulus = modulus.clone() + modulus.clone();
@@ -442,8 +450,7 @@ pub fn compute_double_modulus(modulus: &BigUint, num_bits: usize) -> Vec<BigUint
     limbs
 }
 
-
-pub fn is_prime(modulus: &BigUint) -> bool { 
+pub fn is_prime(modulus: &BigUint) -> bool {
     // we implement a fermat primality test
     //pick a random number less than the modulus
     let mut rng = rand::thread_rng();
